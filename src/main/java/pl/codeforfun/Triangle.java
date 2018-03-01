63f32522315e67032ea195aa6168cfe2d89f1764
LS256 <lstaszew@gmail.com>
2018-03-02 00:13:38 +0100

    Triangle Determine whether a triangle can be built from a given set of edges.

---
+++ src/main/java/pl/codeforfun/Triangle.java
@@ -0,0 +1,31 @@
+package pl.codeforfun;
+
+import java.util.Arrays;
+
+/*
+ * Triangle
+ * Determine whether a triangle can be built from a given set of edges.
+ * Solution got 100% of performance
+ */
+public class Triangle {
+	public Triangle() {
+		
+	}
+	
+	public int doTriangleExist(int[] A) {
+		Arrays.sort(A);
+	        
+	    for(int i=0; i<A.length-3;i++){
+	    	int P = A[i];
+	        int Q = A[i+1];
+	        int R = A[i+2];
+	            
+	        if( (P+Q>R) && (Q+R>P) &&(R+P>Q) ) 
+	        	return 1;
+	        }
+	     return 0;   
+	    }
+	}
+	
+	
+}
