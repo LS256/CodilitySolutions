95beeacb3236412bfb6c7f85c76e1789d135c004
LS256 <lstaszew@gmail.com>
2018-03-21 23:42:30 +0100

    Added first step: loop to collect prefixSums for forward direction with loop to present its values in console.

--- src/main/java/pl/codeforfun/MaxDoubleSliceSum.java
+++ src/main/java/pl/codeforfun/MaxDoubleSliceSum.java
@@ -3,6 +3,15 @@ package pl.codeforfun;
 public class MaxDoubleSliceSum {
 
     public int soution(int[] A){
+    int[] K1 = new int[A.length+1];
+
+    for(int i=1; i<A.length-1; i++){
+        K1[i] = K1[i-1] + A[i];
+    }
+
+    for(int i:K1){
+        System.out.println(i);
+    }
 
     return 1;
     }
