c9300867f2d04b54129db92b1d8b30b96bd5466e
LS256 <lstaszew@gmail.com>
2018-03-21 23:17:21 +0100

    small update of codility solutions

--- src/main/java/pl/codeforfun/MaxDoubleSliceSum.java
+++ src/main/java/pl/codeforfun/MaxDoubleSliceSum.java
@@ -2,10 +2,8 @@ package pl.codeforfun;
 
 public class MaxDoubleSliceSum {
 
-    pulic void soution(){
-        /*
-         *  lines of code
-         */
+    public void soution(){
+        
 
     }
 
--- src/main/java/pl/codeforfun/Triangle.java
+++ src/main/java/pl/codeforfun/Triangle.java
@@ -9,23 +9,20 @@ import java.util.Arrays;
  */
 public class Triangle {
 	public Triangle() {
-		
+
 	}
-	
+
 	public int doTriangleExist(int[] A) {
 		Arrays.sort(A);
-	        
-	    for(int i=0; i<A.length-3;i++){
-	    	int P = A[i];
-	        int Q = A[i+1];
-	        int R = A[i+2];
-	            
-	        if( (P+Q>R) && (Q+R>P) &&(R+P>Q) ) 
-	        	return 1;
-	        }
-	     return 0;   
-	    }
+
+		for (int i = 0; i < A.length - 3; i++) {
+			int P = A[i];
+			int Q = A[i + 1];
+			int R = A[i + 2];
+
+			if ((P + Q > R) && (Q + R > P) && (R + P > Q))
+				return 1;
+		}
+		return 0;
 	}
-	
-	
 }
