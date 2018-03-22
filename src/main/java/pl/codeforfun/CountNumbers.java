efc9c0df6b16037468e3d3e409d7c14cf485d75b
LS256 <lstaszew@gmail.com>
2018-03-22 18:58:59 +0100

    first solution of problem descried in Lesson 10 - CountNumbers. Unfortunately this solution implemet performance app. 33% (n/2) and not assumpted sqrt(N)

--- src/main/java/pl/codeforfun/CountNumbers.java
+++ src/main/java/pl/codeforfun/CountNumbers.java
@@ -5,9 +5,28 @@ public class CountNumbers {
     CountNumbers(){
     }
 
+    /*
+     *  Solution which is not optimal from performance point of view
+     *  it received only 33% of performance - this solution implement not assumpted
+     *  sqrt(n) but n/2
+     *  @param N - number which is tested
+     *  @return result - searched number of factors
+     */
     public int solution(int N){
+        int result =2;
 
-        return 1;
+        if(N==0) {
+            result = 0;
+        } else if(N == 1){
+            result =1;
+        } else {
+            for (int i = N / 2; i > 1; i--) {
+                if ((i * (N / i)) == N) {
+                    result++;
+                }
+            }
+        }
+        return result;
     }
 
 
--- src/test/java/pl/codeforfun/CountNumbersTest.java
+++ src/test/java/pl/codeforfun/CountNumbersTest.java
@@ -15,6 +15,18 @@ public class CountNumbersTest {
 
     @Test
     public void solutionTest(){
+
+        assertEquals(9, cn.solution(100));
+        assertEquals(8, cn.solution(24));
+        assertEquals(6, cn.solution(12));
+        assertEquals(3, cn.solution(9));
+        assertEquals(4, cn.solution(8));
+        assertEquals(4, cn.solution(6));
+        assertEquals(3, cn.solution(4));
+        assertEquals(2, cn.solution(2));
         assertEquals(1, cn.solution(1));
+
+
+
     }
 }
