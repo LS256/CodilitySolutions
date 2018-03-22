043b78fe1fb239bf7c070ea94743f26b65770b8f
LS256 <lstaszew@gmail.com>
2018-03-22 22:47:56 +0100

    Final version of code which got on codility 100/100 - it usues performance sqrt(n)

--- src/main/java/pl/codeforfun/CountNumbers.java
+++ src/main/java/pl/codeforfun/CountNumbers.java
@@ -1,8 +1,12 @@
 package pl.codeforfun;
-
+/*
+ *  @author LS
+ *  @date 2018-03-22
+ *  lesson 10 - count Numbers
+ */
 public class CountNumbers {
 
-    CountNumbers(){
+    CountNumbers() {
     }
 
     /*
@@ -12,13 +16,13 @@ public class CountNumbers {
      *  @param N - number which is tested
      *  @return result - searched number of factors
      */
-    public int solution(int N){
-        int result =2;
+    public int solution(int N) {
+        int result = 2;
 
-        if(N==0) {
+        if (N == 0) {
             result = 0;
-        } else if(N == 1){
-            result =1;
+        } else if (N == 1) {
+            result = 1;
         } else {
             for (int i = N / 2; i > 1; i--) {
                 if ((i * (N / i)) == N) {
@@ -29,7 +33,36 @@ public class CountNumbers {
         return result;
     }
 
+    /*
+     * Solution2 is optimal from performance point of view - got 100/100 on codility.com
+     * In contrast to previous solution it uses min. amount of steps reducing variable from top and bottom
+     * In else condition we have for loop where we know that factors cannot be bigger than N/2
+     * and therefore we are not checking bigger values. from bottom we know that one always is included so that's why
+     * we start checking from 2 incrementing in each next step this variable by one.
+     * When we increment bottom variable then upper variable is equal to N/bottomValue - it gives us feature that upper variable
+     * is decreasing not by one but usually by more. Crucial point of this code snippet are:
+     * - when N is a prime number and we have situation that i == N/i in this case we shouldn't increment our result
+     * - when N has integer sqrt value (like qrt of 36 is six) and then we should increment our result not by two but by one!\
+     *  @param N - number which is tested
+     *  @return result - searched number of factors
+     */
+    public int solution2(int N) {
+        int result = 2;
 
-
+        if (N == 0) {
+            result = 0;
+        } else if (N == 1) {
+            result = 1;
+        } else {
+            for(int i=2; i<=(N/i); i++){
+                if((i == (N/i)) && (i*i == N)){
+                    result++;
+                } else if((i*(N/i)) == N){
+                    result +=2;
+                }
+            }
+        }
+        return result;
+    }
 
 }
--- src/test/java/pl/codeforfun/CountNumbersTest.java
+++ src/test/java/pl/codeforfun/CountNumbersTest.java
@@ -25,8 +25,23 @@ public class CountNumbersTest {
         assertEquals(3, cn.solution(4));
         assertEquals(2, cn.solution(2));
         assertEquals(1, cn.solution(1));
-
-
-
     }
+
+    @Test
+    public void sulution2Test(){
+        assertEquals(9, cn.solution2(100));
+        assertEquals(9, cn.solution2(36));
+        assertEquals(3, cn.solution2(49));
+        assertEquals(2, cn.solution2(41));
+
+        assertEquals(8, cn.solution2(24));
+        assertEquals(6, cn.solution2(12));
+        assertEquals(3, cn.solution2(9));
+        assertEquals(4, cn.solution2(8));
+        assertEquals(4, cn.solution2(6));
+        assertEquals(3, cn.solution2(4));
+        assertEquals(2, cn.solution2(2));
+        assertEquals(1, cn.solution2(1));
+    }
+
 }
