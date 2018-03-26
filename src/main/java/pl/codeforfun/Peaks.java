fb5ab9d7109f9c7bf9dee8edf8a35773a901ff32
LS256 <lstaszew@gmail.com>
2018-03-26 23:33:52 +0200

    Firsts tests and some code snippet

--- src/main/java/pl/codeforfun/Peaks.java
+++ src/main/java/pl/codeforfun/Peaks.java
@@ -6,15 +6,48 @@ public class Peaks {
     }
 
     public int solution(int[] A){
+        int[] peaks = getPeaks(A);
+        int peaksVolume = getPeaksVolume(peaks);
+
+
 
         return 1;
     }
 
+    /*
+     *  method to get max possible size of block base on founded peaks
+     *  @param peaks - array with peaks locations
+     *  @return result - quantity of peaks
+     */
+    public int getPeaksVolume(int[] peaks){
+        int result = 0;
+            for(int i=0; i<peaks.length;i++) {
+                if(peaks[i] == 1) result ++;
+            }
+        return result;
+    }
+
+    /*
+     *  method to get peaks of checked array
+     *  @param A - array where we search peaks
+     *  @return peaks - array with peaks values
+     */
     public int[] getPeaks(int[] A){
         int[] peaks = new int[A.length];
+        int peaksSum =0;
 
-
-        return A;
+        /*
+         * loop where were we check if checked element is a peak.
+        *  if yes then put into new array 1, if not put into new array 0
+        */
+        for(int i=1; i<A.length-1;i++) {
+            if ((A[i - 1] < A[i]) && (A[i] > A[i + 1])) {
+                peaks[i] = 1;
+            } else {
+                peaks[i] = 0;
+            }
+        }
+        return peaks;
     }
 
 
--- src/test/java/pl/codeforfun/PeaksTest.java
+++ src/test/java/pl/codeforfun/PeaksTest.java
@@ -16,6 +16,26 @@ public class PeaksTest {
     @Test
     public void solutionTest(){
         int[] A = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+        assertEquals(1, peaks.solution(A));
 
+    }
+
+    @Test
+    public void getPeaksVolumeTest(){
+        int[] APeaks =  {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0};
+        int[] A2Peaks = {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0};
+
+        assertEquals(3, peaks.getPeaksVolume(APeaks));
+        assertEquals(2, peaks.getPeaksVolume(A2Peaks));
+    }
+    @Test
+    public void getPeaksTest(){
+        int[] A = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+        int[] APeaks = {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0};
+        assertArrayEquals(APeaks, peaks.getPeaks(A));
+
+        int[] A2 =      {1, 2, 3, 2, 1, 2, 3, 5, 3, 2, 1};
+        int[] A2Peaks = {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0};
+        assertArrayEquals(A2Peaks, peaks.getPeaks(A2));
     }
 }
