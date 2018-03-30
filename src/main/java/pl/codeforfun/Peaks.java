7b9b001806b842adbb8522764876d514a7a9d162
LS256 <lstaszew@gmail.com>
2018-03-30 07:42:24 +0200

    peaks solution after adding some tests

--- src/main/java/pl/codeforfun/Peaks.java
+++ src/main/java/pl/codeforfun/Peaks.java
@@ -1,183 +1,77 @@
-<<<<<<< HEAD
 package pl.codeforfun;
 
 import java.util.*;
 
 public class Peaks {
-    Peaks(){
+    Peaks() {
 
     }
 
-/*
- *  This is second solution of peaks issue - and much more correct than first approach
- *  First we create a list wherewe add detected peaks. When we have peaks then we can start cheking
- *  how many flags we can take - in many cases taken flags will be not equal to detected peaks
- *  In main for loop we check possible taken flags by dividing length of given array by flags quantity
- *  and check if peaks are located in these flags intersection
- *  at the end there is another if condition for checking if in given iteration calculated flags quantitty is
- *  is bigger than previously calculated.
- */
-    public int solution2(int[] A) {
-        List<Integer> peaks = new ArrayList<Integer>();
+    /*
+     *  This is second solution of peaks issue - and much more correct than first approach
+     *  First we create a list wherewe add detected peaks. When we have peaks then we can start cheking
+     *  how many flags we can take - in many cases taken flags will be not equal to detected peaks
+     *  In main for loop we check possible taken flags by dividing length of given array by flags quantity
+     *  and check if peaks are located in these flags intersection
+     *  at the end there is another if condition for checking if in given iteration calculated flags quantitty is
+     *  is bigger than previously calculated.
+     */
+    public int solution(int[] A) {
+        List<Integer> peaks = getPeaks(A);
+
         int len = A.length;
 
-        for(int i =1; i<len-1;i++) {
-            if((A[i-1] < A[i]) && (A[i] > A[i+1])) {
-                peaks.add(i);
-            }
-        }
 
         int maxFlags = 0;
 
-        for(int i=1; i<=(len/2); i++) {
-            int blockSize = 1;
-            if((len % i) != 0) continue; else blockSize = len/i;
+        for (int i = 1; i <= (len / 2); i++) {
+            int blockSize;
 
-            int blockStart = 0;
-            int blockStop = blockSize;
-
-            int flagsOnPeaks = 0;
-
-            for(Integer peak:peaks) {
-
-                if(peak>=blockStart && peak<blockStop){
-                    flagsOnPeaks++;
-                    blockStart += blockSize;
-                    blockStop += blockSize;
-                }
+            if ((len % i) != 0) {
+                continue;
+            }else {
+                blockSize = len / i;
             }
-            if(flagsOnPeaks == i) maxFlags = Math.max(maxFlags, flagsOnPeaks);
+
+            int flagsOnPeaks = getFlagsOnPeaks(peaks, blockSize);
+
+            if (flagsOnPeaks == i) maxFlags = Math.max(maxFlags, flagsOnPeaks);
         }
 
         return maxFlags;
     }
 
+    public int getFlagsOnPeaks(List<Integer> peaks, int blockSize) {
+        //  block start and blockStop describe intersection for searched peak valeu
+        int blockStart = 0;
+        int blockStop = blockSize;
+        //  tempVariable to keep founded peaks
+        int flagsOnPeaks = 0;
 
-    //  TODO write a code to  search block size like 20%6 !=0
-
-    public int solution(int[] A){
-        int[] peaks = getPeaks(A);
-        int peaksVolume = getPeaksVolume(peaks);
-        int blockSize = getBlockSize(peaks, peaksVolume);
-
-        int pos =0;
-        int tempBlockSize = 0;
-        while(pos < peaks.length){
-            if((peaks[pos]>0) && (tempBlockSize < blockSize)){
-               pos = ((peaks.length / blockSize) - ((peaks.length - pos - 1) / blockSize)) * blockSize;
-               tempBlockSize = 0;
- //              System.out.println("blockSize: " + blockSize + "  pos: " + pos);
-            } else {
-                pos++;
-                tempBlockSize++;
+        for (Integer peak : peaks) {
+            if (peak >= blockStart && peak < blockStop) {
+                flagsOnPeaks++;
+                blockStart += blockSize;
+                blockStop += blockSize;
             }
-
-            if(tempBlockSize >= blockSize){
-                tempBlockSize = 0;
-                pos = 0;
-                blockSize++;
-                while((peaks.length % blockSize) != 0) {
-                    blockSize++;
-                }
-            }
-
-
         }
-        return (peaks.length / blockSize);
-    }
-
-
-    /*
-     *  getting possible size of block base on founded peaks
-     *  @param
-     *  @return
-     */
-    public int getBlockSize(int[] peaks, int peaksVolume){
-        int preBlockSize = peaksVolume;
-
-        while(( peaks.length % preBlockSize) != 0){
-            preBlockSize--;
-        }
-        return (peaks.length / preBlockSize);
+        return flagsOnPeaks;
     }
 
     /*
-     *
-     *  @param peaks - array with peaks locations
-     *  @return result - quantity of peaks
+     *  Method to get values of peaks locations and add them to the list
+     *  @param A - array with given values. In this array we will search for peaks
+     *  @return peaks - list with values of peaks locations
      */
-    public int getPeaksVolume(int[] peaks){
-        int result = 0;
-            for(int i=0; i<peaks.length;i++) {
-                if(peaks[i] == 1) result ++;
-            }
-        return result;
-    }
+    public List<Integer> getPeaks(int[] A) {
+        List<Integer> peaks = new ArrayList<Integer>();
+        int len = A.length;
 
-    /*
-     *  method to get peaks of checked array
-     *  @param A - array where we search peaks
-     *  @return peaks - array with peaks values
-     */
-    public int[] getPeaks(int[] A){
-        int[] peaks = new int[A.length];
-        int peaksSum =0;
-
-        /*
-         * loop where were we check if checked element is a peak.
-        *  if yes then put into new array 1, if not put into new array 0
-        */
-        for(int i=1; i<A.length-1;i++) {
+        for (int i = 1; i < len - 1; i++) {
             if ((A[i - 1] < A[i]) && (A[i] > A[i + 1])) {
-                peaks[i] = 1;
-            } else {
-                peaks[i] = 0;
+                peaks.add(i);
             }
         }
         return peaks;
     }
-
-
-
 }
-=======
-import java.util.*;
-
-class Solution {
-    public int solution(int[] A) {
-        List<Integer> peaks = new ArrayList<Integer>();
-        int len = A.length;
-        
-        for(int i =1; i<len-1;i++) {
-            if((A[i-1] < A[i]) && (A[i] > A[i+1])) {
-                peaks.add(i);
-            }
-        }
-       if(peaks.size() == 0) return 0;
-       
-       int maxFlags = 0;
-       
-       for(int i=1; i<=(len/2); i++) {
-           int blockSize = 1;
-           if((len % i) != 0) continue; else blockSize = len/i;
-           
-           int blockStart = 0;
-           int blockStop = blockSize;
-           
-           int flagsOnPeaks = 0;
-           
-           for(Integer peak:peaks) {
-
-               if(peak>=blockStart && peak<blockStop){
-                   flagsOnPeaks++;
-                   blockStart += blockSize;
-                   blockStop += blockSize;
-               }
-           }
-           if(flagsOnPeaks == i) maxFlags = Math.max(maxFlags, flagsOnPeaks);
-       }
-   
-       return maxFlags;
-    }
-} 
->>>>>>> b04aee93ac76c4da3ccf6d912724832029820022
--- src/test/java/pl/codeforfun/PeaksTest.java
+++ src/test/java/pl/codeforfun/PeaksTest.java
@@ -2,77 +2,80 @@ package pl.codeforfun;
 
 import org.junit.Before;
 import org.junit.Test;
+import java.util.*;
+
 
 import static org.junit.Assert.*;
 
 public class PeaksTest {
     private Peaks peaks;
 
+    int[] A = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+    int[] A2 = {1, 5, 1, 5, 1, 5, 1, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1};
+    int[] A3 = {1, 5, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int[] A4 = {3,4,3};
+    int[] A5 = {1, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2};
+    int[] A6 = {1, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 3, 2};
+
+    List<Integer> AList = new ArrayList<Integer>(Arrays.asList(3,5,10));
+    List<Integer> A2List = new ArrayList<Integer>(Arrays.asList(1, 3, 5, 7, 15));
+    List<Integer> A3List = new ArrayList<Integer>(Arrays.asList(1, 3));
+    List<Integer> A4List = new ArrayList<Integer>(Arrays.asList(1));
+    List<Integer> A5List = new ArrayList<Integer>(Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15));
+    List<Integer> A6List = new ArrayList<Integer>(Arrays.asList(1, 3, 5, 7, 9, 11, 13, 16));
+
     @Before
+    //  Inform JUnit that this method should run before each test
     public void setUp() throws Exception {
         peaks = new Peaks();
     }
 
     @Test
-    public void solution2Test() {
-        int[] A = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
-        assertEquals(3, peaks.solution2(A));
-
-        int[] A2 = {1, 5, 1, 5, 1, 5, 1, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1};
-        assertEquals(2, peaks.solution2(A2));
-
-        int[] A3 = {1, 5, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        assertEquals(1, peaks.solution2(A3));
-
-        int[] A4 = {3,4,3};
-        assertEquals(1,peaks.solution2(A4));
-
-        int[] A5 = {1,3,2,3,2,3,2,3,2,3,2,3,2,3,2,3,2};
-        assertEquals(1,peaks.solution2(A5));
-
-        int[] A6 = {1,3,2,3,2,3,2,3,2,3,2,3,2,3,2,2,3,2};
-        assertEquals(6,peaks.solution2(A6));
-
-    }
-
-    @Test
-    public void solutionTest(){
-        int[] A = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+    public void solutionTest() {
         assertEquals(3, peaks.solution(A));
-
-        int[] A2 = {1, 5, 1, 5, 1, 5, 1, 5, 4, 3, 2, 1, 2, 3, 4, 5, 4, 3, 2, 1};
         assertEquals(2, peaks.solution(A2));
-
-        int[] A3 = {1, 5, 1, 9, 8, 7, 6, 5, 4, 3, 2, 1};
         assertEquals(1, peaks.solution(A3));
-    }
-
-    @Test
-    public void getBlockSizeTest(){
-        int[] APeaks =  {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0};
-        int[] A2Peaks = {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0};
-
-        assertEquals(4, peaks.getBlockSize(APeaks, peaks.getPeaksVolume(APeaks)));
-        assertEquals(11, peaks.getBlockSize(A2Peaks, peaks.getPeaksVolume(A2Peaks)));
-    }
-
-    @Test
-    public void getPeaksVolumeTest(){
-        int[] APeaks =  {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0};
-        int[] A2Peaks = {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0};
-
-        assertEquals(3, peaks.getPeaksVolume(APeaks));
-        assertEquals(2, peaks.getPeaksVolume(A2Peaks));
+        assertEquals(1,peaks.solution(A4));
+        assertEquals(1,peaks.solution(A5));
+        assertEquals(6,peaks.solution(A6));
     }
 
     @Test
     public void getPeaksTest(){
-        int[] A = {1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
-        int[] APeaks = {0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0};
-        assertArrayEquals(APeaks, peaks.getPeaks(A));
 
-        int[] A2 =      {1, 2, 3, 2, 1, 2, 3, 5, 3, 2, 1};
-        int[] A2Peaks = {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0};
-        assertArrayEquals(A2Peaks, peaks.getPeaks(A2));
+
+        assertEquals(AList, peaks.getPeaks(A));
+        assertEquals(A2List, peaks.getPeaks(A2));
+        assertEquals(A3List, peaks.getPeaks(A3));
+        assertEquals(A4List, peaks.getPeaks(A4));
+        assertEquals(A5List, peaks.getPeaks(A5));
+        assertEquals(A6List, peaks.getPeaks(A6));
+
     }
+
+    @Test
+    public void getFlagsOnPeaksTest() {
+        assertEquals(3, peaks.getFlagsOnPeaks(AList, 4));
+        assertEquals(2, peaks.getFlagsOnPeaks(AList, 6));
+        assertNotEquals(2, peaks.getFlagsOnPeaks(AList, 4));
+
+
+        assertEquals(2, peaks.getFlagsOnPeaks(A2List, 10));
+        assertEquals(1, peaks.getFlagsOnPeaks(A2List, 20));
+        assertNotEquals(1, peaks.getFlagsOnPeaks(A2List, 4));
+
+        assertEquals(1, peaks.getFlagsOnPeaks(A3List, 12));
+        assertNotEquals(2, peaks.getFlagsOnPeaks(A3List, 12));
+
+        assertEquals(1, peaks.getFlagsOnPeaks(A4List, 3));
+        assertNotEquals(2, peaks.getFlagsOnPeaks(A4List, 3));
+
+        assertEquals(1, peaks.getFlagsOnPeaks(A5List, 17));
+        assertNotEquals(2, peaks.getFlagsOnPeaks(A4List, 17));
+
+        assertEquals(6, peaks.getFlagsOnPeaks(A6List, 3));
+        assertEquals(3, peaks.getFlagsOnPeaks(A6List, 6));
+        assertNotEquals(2, peaks.getFlagsOnPeaks(A6List, 6));
+    }
+
 }
