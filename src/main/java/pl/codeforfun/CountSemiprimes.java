1bf4f875d2baef12452cd921950fdf2b0a06cded
LS256 <lstaszew@gmail.com>
2018-04-05 01:05:07 +0200

    Todays commit include solution to problem described in lesson 11 called CountSemiprimes'. This solution got 100/100 in codility tests

--- src/main/java/pl/codeforfun/CountSemiprimes.java
+++ src/main/java/pl/codeforfun/CountSemiprimes.java
@@ -1,4 +1,78 @@
 package pl.codeforfun;
 
 public class CountSemiprimes {
+
+
+    public int[] solution(int N, int[] P, int[] Q){
+        boolean[] primes = getPrimes(N);
+        boolean[] semiprimes = getSemiprimes(N, primes);
+        int[] factors = getFactors(N, semiprimes);
+
+        int[] results = new int[P.length];
+        for(int i=0; i<P.length;i++){
+            results[i] = factors[Q[i]] - factors[P[i]-1];
+        }
+        return results;
+    }
+
+    public int[] getFactors(int N, boolean[] semiprimes){
+        int[] factors = new int[N+1];
+
+        int tempVal =0;
+        for(int i =0; i<=N;i++){
+            if(semiprimes[i]){
+                tempVal++;
+            }
+            factors[i] = tempVal;
+        }
+        return factors;
+    }
+
+
+
+
+
+
+    public boolean[] getSemiprimes(int N, boolean[] primes){
+        boolean[] semiprimes = new boolean[N+1];
+
+        for(int i=2; i<=N/2; i++) {
+            if (!primes[i]) {
+                int j = i;
+                while (i * j <= N) {
+                    if (!primes[j]) {
+                        semiprimes[j * i] = true;
+                    }
+                    j++;
+                }
+            }
+        }
+        return semiprimes;
+    }
+
+
+
+    public boolean[] getPrimes(int N) {
+        boolean[] primes = new boolean[N];
+
+        int i =2;
+
+        while(i*i < N){
+            int k = i+i;
+            while(k<N){
+
+                primes[k] = true;
+                k+=i;
+            }
+
+            i++;
+            while(primes[i]){
+                i++;
+            }
+        }
+
+        return primes;
+    }
+
+
 }
--- src/test/java/pl/codeforfun/CountSemiprimesTest.java
+++ src/test/java/pl/codeforfun/CountSemiprimesTest.java
@@ -1,4 +1,58 @@
 package pl.codeforfun;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class CountSemiprimesTest {
+
+    private CountSemiprimes countSemiprimes;
+
+    @Before
+    public void testInit(){
+        countSemiprimes = new CountSemiprimes();
+    }
+
+    @Test
+    public void getPrimesTest(){
+        boolean[] A = {false, false, false, false, true, false, true, false, true, true, true, false};
+        assertArrayEquals(A, countSemiprimes.getPrimes(12));
+    }
+
+    @Test
+    public void getSemiprimesTest(){
+        boolean[] A = { false, false, false,false,true, false, true, false, false, true, true, false,
+                        false, false, true, true, false, false, false, false, false, true, true, false,
+                        false, true, true};
+        boolean[] A2 = {false, false, false, false, true, false, true, false, true, true, true, false,
+                        true, false, true, true, true, false, true, false, true, true, true, false, true, true};
+        assertArrayEquals(A, countSemiprimes.getSemiprimes(26, A2));
+    }
+
+    @Test
+    public void getFactorsTest(){
+        int[] A = {0,0,0,0,1,1,2,2,2,3,4,4,4,4,5,6,6,6,6,6,6,7,8,8,8,9,10};
+//        boolean[] A2 = {false, false, false, false, true, false, true, false, true, true, true, false,
+//                true, false, true, true, true, false, true, false, true, true, true, false, true, true};
+
+        boolean[] A2 = {false, false, false, false, true, false, true, false, false, true, true, false, false, false, true, true,
+                false, false, false, false, false, true, true, false, false, true, true};
+
+        assertArrayEquals(A, countSemiprimes.getFactors(26, A2));
+    }
+
+    @Test
+    public void solutionTest(){
+        int[] P = { 1, 4, 16};
+        int[] Q = {26,10, 20};
+        int   N = 26;
+        int[] testResult = {10,4,0};
+
+        assertArrayEquals(testResult, countSemiprimes.solution(N, P, Q));
+    }
+
 }
+
+
